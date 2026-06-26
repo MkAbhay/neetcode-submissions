f29456eef2f53c207de99d9eea0b0508106d73e7
Abhay Mankari <93965022+MkAbhay@users.noreply.github.com>
2026-06-26 11:06:04 +0530

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        return this.formula_method(nums)
+    }
+
+    formula_method (nums) {
+        const n = nums.length;
+        const sum = (n * (n+1))/2;
+        const num_sum = nums.reduce((acc, cur) => {
+            acc = acc + cur
+            return acc
+        }, 0);
+        console.log(sum, num_sum)
+        return sum - num_sum
+    }
+
+    sort_method (nums) {
+        nums.sort()
+        let previous = nums[0];
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] -1 !== previous) {
+                return nums[i] -1
+            }
+            previous = nums[i]
+        }
+        return nums[0] === 0 ? nums.length : 0
+    }
+}
