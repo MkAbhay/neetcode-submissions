6028bee05cbd26fae203fcb6412933aa2bb12bc2
Abhay Mankari <93965022+MkAbhay@users.noreply.github.com>
2026-06-27 15:00:17 +0530

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        return this.prefix_suffix(nums)
+    }
+
+    prefix_suffix (nums) {
+        let pre = []
+        let temp = 1;
+        for (let i = 0; i < nums.length; i++) {
+            let product = temp*nums[i]
+            pre.push(product)
+            temp = product
+        }
+        let suf = []
+        temp = 1
+        for (let i = nums.length-1; i >= 0; i--) {
+            let product = temp*nums[i]
+            suf.push(product)
+            temp = product
+        }
+        suf.reverse()
+
+        return nums.map((x, i) => (pre[i-1] ?? 1) * (suf[i+1] ?? 1) )
+    }
+
+    division (nums) {
+        let result = [];
+        let product = nums.filter(x => x !== 0).reduce((acc, cur) => {
+            acc = acc * cur
+            return acc
+        }, 1)
+        let zero = nums.filter(x => x === 0)
+
+        if (zero.length > 1) {
+            result = nums.map(x => 0)
+        } else if (zero.length === 1) {
+            result =  nums.map(x => x === 0 ? product : 0)
+        } else {
+            result = nums.map(x => product/x)
+        }        
+
+        return result
+    }
+}
