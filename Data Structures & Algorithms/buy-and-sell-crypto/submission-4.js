546191a1d83773bb2e1995b1f6694fcbde695aa8
Abhay Mankari <93965022+MkAbhay@users.noreply.github.com>
2026-06-27 21:25:05 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        return this.greedy(prices)
+    }
+
+    greedy (prices) {
+        let max_sell = 0;
+        let max_profit = 0;
+        for (let i = prices.length-1; i >= 0; i--) {
+            if (prices[i] > max_sell) {
+                max_sell = prices[i]
+                continue;
+            }
+            let cur_profit = max_sell - prices[i]
+            max_profit = Math.max(cur_profit, max_profit)
+        }
+        return max_profit
+    }
+
+    brute_force (prices) {
+        let max_profit = 0;
+        for (let i = 0; i < prices.length; i++) {
+            for (let j = i + 1; j < prices.length; j++) {
+                let current_profit = prices[j] - prices[i]
+                max_profit = Math.max(max_profit, current_profit)
+            }
+        }
+        return max_profit
+    }
+}
