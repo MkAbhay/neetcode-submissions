8777ed0bf161877283dfd398b48db9ab09aebc22
Abhay Mankari <93965022+MkAbhay@users.noreply.github.com>
2026-07-02 00:04:20 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-17

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-17.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        return this.binary_serach(nums)
+    }
+
+    binary_serach (nums) {
+        let start  = 0
+        let end = nums.length -1
+        while (start <= end) {
+            let mid = start + Math.floor((end - start) / 2) 
+            if (nums[0] < nums[mid])   start = mid + 1
+            else if (nums[0] > nums[mid]) {
+                if (nums[mid-1] > nums[mid])    return nums[mid]
+                end = mid - 1
+            }
+            else    return Math.min(nums[mid], nums[end])
+        }
+
+        return nums[0]
+    }
+
+    O_N (nums) {
+        let low = nums[0]
+        for (let i = 1; i < nums.length; i++) {
+            low = Math.min(nums[i], low)
+        }
+        return low
+    }
+}
