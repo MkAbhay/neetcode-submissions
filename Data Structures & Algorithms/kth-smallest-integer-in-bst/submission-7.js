d7f5cd9104ab9303e407f992cb1b7ce65b8496cb
Abhay Mankari <93965022+MkAbhay@users.noreply.github.com>
2026-07-15 17:50:38 +0530

    Add: kth-smallest-integer-in-bst - submission-7

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-7.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} k
+     * @return {number}
+     */
+    kthSmallest(root, k) {
+        return this.dfs(root, k)
+    }
+
+    dfs (root, k) {
+        let arr = [ k, null]
+        traversal(root, arr)
+        return arr[1]
+
+        function traversal (root, arr) {
+            if (!root)  return
+            traversal(root.left, arr)
+            arr[0]--
+            if (arr[0] === 0) {
+                arr[1] = root.val
+                return
+            }
+            traversal(root.right, arr)
+        }
+    }
+
+    dfs_array (root, k) {
+        let arr = []
+        traversal(root, arr)
+        arr.sort((a, b) => b - a)
+        return arr[arr.length - k]
+
+        function traversal (root, arr) {
+            if (!root)  return
+            traversal(root.left, arr)
+            arr.push(root.val)
+            traversal(root.right, arr)
+        };
+    }
+}
