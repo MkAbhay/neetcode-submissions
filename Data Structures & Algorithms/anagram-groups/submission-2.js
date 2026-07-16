3d710b59ce97461715694026bf04f5a90e9f6761
Abhay Mankari <93965022+MkAbhay@users.noreply.github.com>
2026-07-16 16:52:14 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        return this.sort(strs)
+    }
+
+    non_sort (strs) {
+        let map = new Map()
+        
+        for (let i = 0; i < strs.length; i++) {
+            let count = new Array(26).fill(0);
+            for (let j = 0; j < strs[i].length; j++) {
+                count[strs[i].charCodeAt(j) - 97]++
+            }
+            let key = count.join("")
+            
+            let arr = map.get(key) || []
+            map.set(key, [...arr, strs[i]])
+        }
+
+        return [...map.values()]
+    }
+
+    sort (strs) {
+        const map = new Map();
+
+        for (const word of strs){
+            let key = word.split("").sort().join("");
+            let val_arr = map.get(key) || []
+            val_arr.push(word)
+            map.set(key, val_arr)
+        }
+
+        return [...map.values()]
+    }
+}
